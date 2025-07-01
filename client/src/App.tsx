c13ae348c348eacf3870a193fa71a768846691f2
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2025-07-02 01:41:11 +0300

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Resolver } from './components/Resolver';
+
+function App() {
+  return (
+    <div className="App">
+      <Resolver />
+    </div>
+  );
+}
+
+export default App;
